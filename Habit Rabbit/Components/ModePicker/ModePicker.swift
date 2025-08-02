02a0046acb1dd192fbfadbc52f5c69a5d1dc97c5
Berken Sayilir <sayilir.berken@gmail.com>
2025-08-02 13:45:00 +0200

    .

--- Habit Rabbit/Components/ModePicker/ModePicker.swift
+++ Habit Rabbit/Components/ModePicker/ModePicker.swift
@@ -6,9 +6,7 @@ extension Habit.Dashboard {
         
         var width: CGFloat
         @Binding var mode: Habit.Card.Mode
-        
-        typealias Mode = Habit.Card.Mode
-        
+                
         var body: some View {
             HStack(spacing: 0) {
                 ForEach(Habit.Card.Mode.allCases, id: \.self) { item in
@@ -31,7 +29,7 @@ extension Habit.Dashboard {
                     .fill(.quaternary)
                     .frame(width: width / 3)
                     .frame(maxWidth: .infinity, alignment: .leading)
-                    .offset(x: CGFloat(Mode.allCases.firstIndex(of: mode) ?? 0) * (width / 3), y: 0)
+                    .offset(x: CGFloat(Habit.Card.Mode.allCases.firstIndex(of: mode) ?? 0) * (width / 3), y: 0)
                     .animation(.spring(duration: 0.62), value: mode)
             }
             .frame(width: width)
