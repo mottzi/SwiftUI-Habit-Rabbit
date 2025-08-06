8507b2b4ca626b815af1237203d05df66aac66bd
Berken Sayilir <sayilir.berken@gmail.com>
2025-08-07 01:15:20 +0200

    .

--- Habit Rabbit/Components/Dashboard/HabitDetailView.swift
+++ Habit Rabbit/Components/Dashboard/HabitDetailView.swift
@@ -3,10 +3,14 @@ import SwiftUI
 
 struct HabitDetailView: View {
     
+    @Environment(Habit.Dashboard.Manager.self) var dashboardManager
     @Environment(Habit.Card.Manager.self) var cardManager
     
     var body: some View {
-        Text(cardManager.name)
+        Habit.Card()
+            .environment(dashboardManager)
+            .environment(cardManager)
+            .containerRelativeFrame(.horizontal, count: 2, spacing: 16)        
     }
     
 }
