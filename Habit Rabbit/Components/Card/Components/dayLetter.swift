4d916dadfe7ade36144dbe781ca048918294d754
Berken Sayilir <sayilir.berken@gmail.com>
2025-08-08 13:06:20 +0200

    ref

--- Habit Rabbit/Components/Card/Components/dayLetter.swift
+++ Habit Rabbit/Components/Card/Components/dayLetter.swift
@@ -3,11 +3,10 @@ import SwiftUI
 extension Habit.Card {
     
     func dayLetter(for date: Date, color: Color) -> some View {
-        Text(date.weekday)
-            .font(.caption2)
-            .fontWeight(.medium)
-            .foregroundStyle(color)
-            .lineLimit(1)
+        dayLetter(
+            symbol: date.weekday,
+            color: color
+        )
     }
     
     func dayLetter(symbol: String, color: Color) -> some View {
