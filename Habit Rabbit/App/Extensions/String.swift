b64044dd0fb4ab377c262c7a351312cdb02c30bc
Berken Sayilir <sayilir.berken@gmail.com>
2025-08-08 14:55:24 +0200

    ref

--- Habit Rabbit/App/Extensions/String.swift
+++ Habit Rabbit/App/Extensions/String.swift
@@ -8,11 +8,12 @@ extension String {
     
     static func pluralize(string: String, count: Int) -> String {
         let count = count == 0 ? 2 : count
-        let attributedString = AttributedString(localized: "^[\(count) \(string)](inflect: true)")
-        let localizedString = String(attributedString.characters)
-        let countPrefix = "\(count) "
-        guard localizedString.hasPrefix(countPrefix) else { return localizedString }
-        return String(localizedString.dropFirst(countPrefix.count))
+        let query = LocalizationValue("^[\(count) \(string)](inflect: true)")
+        let attributed = AttributedString(localized: query)
+        let localized = String(attributed.characters)
+        let prefix = "\(count) "
+        guard localized.hasPrefix(prefix) else { return localized }
+        return String(localized.dropFirst(prefix.count))
     }
 }
 
