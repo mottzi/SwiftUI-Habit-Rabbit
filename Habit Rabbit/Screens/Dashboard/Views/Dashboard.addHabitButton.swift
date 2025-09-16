f547399e8fcbd5a9bfc89b67ca4765fcbd811480
Berken Sayilir <sayilir.berken@gmail.com>
2025-09-16 13:09:12 +0200

    add button liquid glass

--- Habit Rabbit/Screens/Dashboard/Views/Dashboard.addHabitButton.swift
+++ Habit Rabbit/Screens/Dashboard/Views/Dashboard.addHabitButton.swift
@@ -2,21 +2,41 @@ import SwiftUI
 
 extension Habit.Dashboard {
     
+    @ViewBuilder
     var addHabitButton: some View {
-        Button {
-            presentAddSheet = true
-        } label: {
-            Image(systemName: "plus")
-                .font(.title)
-                .fontWeight(.medium)
-                .foregroundStyle(colorScheme == .light ? .black : .white)
-                .frame(width: 64, height: 64)
-                .background { Habit.Card.Background(in: .circle, material: .ultraThinMaterial) }
-                .padding()
+        if #available(iOS 26, *) {
+            Button {
+                presentAddSheet = true
+            } label: {
+                Image(systemName: "plus")
+                    .font(.title)
+                    .fontWeight(.medium)
+//                    .foregroundStyle(colorScheme == .light ? .black : .white)
+//                    .frame(width: 64, height: 64)
+//                    .background { Habit.Card.Background(in: .circle, material: .ultraThinMaterial) }
+                    .padding()
+                    .glassEffect()
+            }
+            .buttonStyle(.plain)
+            .sensoryFeedback(.selection, trigger: presentAddSheet)
+            .offset(x: -12, y: 0)
+        } else {
+            Button {
+                presentAddSheet = true
+            } label: {
+                Image(systemName: "plus")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundStyle(colorScheme == .light ? .black : .white)
+                    .frame(width: 64, height: 64)
+                    .background { Habit.Card.Background(in: .circle, material: .ultraThinMaterial) }
+                    .padding()
+            }
+            .buttonStyle(.plain)
+            .sensoryFeedback(.selection, trigger: presentAddSheet)
+            .offset(x: 6, y: 0)
         }
-        .buttonStyle(.plain)
-        .sensoryFeedback(.selection, trigger: presentAddSheet)
-        .offset(x: 6, y: 0)
+        
     }
     
 }
